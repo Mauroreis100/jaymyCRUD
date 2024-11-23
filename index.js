d34b7e517a7d62bdf6b0760c01be58f1d39ee1ea
Mauroreis100 <maurodosreis71@yahoo.com>
2024-11-23 18:30:41 +0100

    Search local storage

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function removeAllChildNodes(parent) {
   }
 }
 
-let sessionBD=[{}]
+let sessionBD = [{}]
 async function fetchIMDBData(name) {
   if (name) {
     query = name;
@@ -26,79 +26,109 @@ async function fetchIMDBData(name) {
     if (!response.ok) {
       throw new Error();
     }
+
+
+
     const data = await response.json();
     console.log('Fetched Data:', data);
     const movieData = data.result;
     sessionStorage.setItem('movies', JSON.stringify(movieData))
     sessionBD = JSON.parse(sessionStorage.getItem('movies') || '[]');
-
+    listIMDBData()
   } catch (error) {
     console.error('Error fetching data:', error);
   }
 }
 
-function listIMDBData(){
+function listIMDBData() {
   let cards = []
-sessionBD = JSON.parse(sessionStorage.getItem('movies') || '[]');
-console.log('Movies:', sessionBD);
-
-// if (sessionBD.length != 0) {
-//   sessionBD.forEach(movie => {
-//     const card = document.createElement('div');
-//     card.className = "movie-card";
-//     card.innerHTML = `
-//       <div class="card" style="width: 18rem;">
-//         <a href=${movie.Poster} target="_blank"><img class="card-img-top" src=${movie.Poster} alt="Card image cap"></a>
-//         <div class="card-body">
-//           <p class="card-text"><h1>${movie.Title}</h1><h3>${movie.Year}</h3></p>
-//         </div>
-//       </div>
-//     `;
-
-//     cards.push(card)
-
-//   });
-
-// } else {
-//   const errorView = document.createElement('div');
-//   errorView.errorView = "movie-card";
-//   errorView.innerHTML = `
-//       <div class="error-message" style="position: center;">
-//         <h1>Movie Not found :( ${name}</h1>
-//       </div>
-//     `;
-//   cards.push(errorView)
-//   console.log(`No media encountered named ${name}`)
-// }
-
-// let list = document.getElementById("list")
-// removeAllChildNodes(list)
-// cards.forEach(card => {
-//   list.appendChild(card, list.firstChild);
-// });
-
-
-
-
-}
-
-
-
-function searchLocal(name){
   sessionBD = JSON.parse(sessionStorage.getItem('movies') || '[]');
-  console.log('Searcg local:', sessionBD[0].Title);
-  sessionBD.forEach(movies => {
-    if (movies.Title==name) {
-      console.log("Found")
-  
-    } else {
+  console.log('Movies:', sessionBD);
 
-    }
- });
+  if (sessionBD.length != 0) {
+    sessionBD.forEach(movie => {
+      const card = document.createElement('div');
+      card.className = "movie-card";
+      card.innerHTML = `
+      <div class="card" style="width: 18rem;">
+        <a href=${movie.Poster} target="_blank"><img class="card-img-top" src=${movie.Poster} alt="Card image cap"></a>
+        <div class="card-body">
+          <p class="card-text"><h1>${movie.Title}</h1><h3>${movie.Year}</h3></p>
+        </div>
+      </div>
+    `;
+
+      cards.push(card)
+
+    });
+
+  } else {
+    const errorView = document.createElement('div');
+    errorView.errorView = "movie-card";
+    errorView.innerHTML = `
+      <div class="error-message" style="position: center;">
+        <h1>Movie Not found :( ${name}</h1>
+      </div>
+    `;
+    cards.push(errorView)
+    console.log(`No media encountered named ${name}`)
+  }
+
+  let list = document.getElementById("list")
+  removeAllChildNodes(list)
+  cards.forEach(card => {
+    list.appendChild(card, list.firstChild);
+  });
+
+
+
 }
 
 
-function fetchLocal(){
+
+function searchLocal(name) {
+  sessionBD = JSON.parse(sessionStorage.getItem('movies') || '[]');
+  let cards = []
+  let found = false
+  sessionBD.forEach(movies => {
+    if (movies.Title.includes(name)) {
+      console.log("Encontrado" + movies.Title)
+      const card = document.createElement('div');
+      card.className = "movie-card";
+      card.innerHTML = `
+      <div class="card" style="width: 18rem;">
+        <a href=${movies.Poster} target="_blank"><img class="card-img-top" src=${movies.Poster} alt="Card image cap"></a>
+        <div class="card-body">
+          <p class="card-text"><h1>${movies.Title}</h1><h3>${movies.Year}</h3></p>
+        </div>
+      </div>
+    `;
+
+      cards.push(card)
+    }
+  });
+  if (cards==0) {
+      const errorView = document.createElement('div');
+      errorView.errorView = "movie-card";
+      errorView.innerHTML = `
+          <div class="error-message" style="position: center;">
+            <h1>Movie Not found :( ${name}</h1>
+          </div>
+        `;
+      cards.push(errorView)
+      console.log(`No media encountered named ${name}`)
+    
+      
+    }
+    let list = document.getElementById("list")
+    removeAllChildNodes(list)
+    cards.forEach(card => {
+      list.appendChild(card, list.firstChild);
+    });
+}
+
+
+function fetchLocal() {
   /*
       if (movieData != 0) {
       movieData.forEach(movie => {
@@ -151,10 +181,8 @@ function addIMBDData() {
     Poster: "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg"
   }
 
-  sessionStorage.setItem('movies',newMovie)
+  sessionStorage.setItem('movies', newMovie)
 }
 fetchIMDBData();
-listIMDBData()
-searchLocal('The Avengers')
 addIMBDData()
 
